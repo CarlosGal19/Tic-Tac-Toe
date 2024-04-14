278b96d3bd1ee3c15d182600e16aa77b07c8b0ea
Carlos Alejandro Galindo Islas <utm22030587@utma.edu.mx>
2024-04-14 17:32:45 -0600

    feat: create square component and render it

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,39 @@
-// import { useState } from 'react'
+import { useState } from 'react'
 import './App.css'
+
+
+// Square component that will be used to render each square of the board
+const Square = ({ children, updateBoard, index }) => {
+  return (
+    <div className='square'>
+      {children}
+    </div>
+  )
+}
 
 function App() {
 
+  // Create a state variable to hold the board state
+  const [board, setBoard] = useState(['x', 'o', 'x', 'o', 'x', 'o', 'x', 'o', 'x']);
+
   return (
     <>
-      <h1>Tic-Tac-Toe</h1>
+      <main className='board'>
+        <h1>Tic-Tac-Toe</h1>
+        <section className='game'>
+          {
+            // Loop through the board state and render a Square component for each square
+            board.map((a, index) => {
+              return (
+                <Square key={index} index={index}>
+                  {board[index]}
+                </Square>
+              )
+
+            })
+          }
+        </section>
+      </main>
     </>
   )
 }
